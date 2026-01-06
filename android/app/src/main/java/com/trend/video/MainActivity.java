9f6c98a5b994484c3b57d1aa310c03e3090612cc
LolHubFun <lolhubfun@proton.me>
2026-01-07 00:02:43 +0400

    fixed BOM and compilation error in MainActivity

--- android/app/src/main/java/com/trend/video/MainActivity.java
+++ android/app/src/main/java/com/trend/video/MainActivity.java
@@ -1,14 +1,13 @@
-﻿package com.trend.video;
+package com.trend.video;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.capgo.capacitor_ffmpeg.FFmpeg; // Mühərriki bura gətiririk
+import com.capgo.capacitor_ffmpeg.FFmpeg;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Mühərriki körpüyə əllə əlavə edirik
         registerPlugin(FFmpeg.class);
     }
 }
